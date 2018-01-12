35517a93844f19ba347493b061781e4862292034
Briant Oliveira <briantmoliveira@gmail.com>
2018-01-12 02:13:33 -0800

    recursion done

--- source/palindromes.py
+++ source/palindromes.py
@@ -13,8 +13,8 @@ def is_palindrome(text):
     # implement is_palindrome_iterative and is_palindrome_recursive below, then
     # change this to call your implementation to verify it passes all tests
     assert isinstance(text, str), 'input is not a string: {}'.format(text)
-    return is_palindrome_iterative(text)
-    # return is_palindrome_recursive(text)
+    #return is_palindrome_iterative(text)
+    return is_palindrome_recursive(text)
 
 
 
@@ -24,26 +24,52 @@ def is_palindrome_iterative(text):
     # once implemented, change is_palindrome to call is_palindrome_iterative
     # to verify that your iterative implementation passes all tests
 
+    # If the word is bigger then 1 its a palindrome
     if len(text) <= 1:
         return True
 
+    #Continue looping through the first and last parts
     else:
         left = 0
         right = len(text) -1
         while left < right:
             if text[left] == text[right]:
+                #Increase to do the next step
                 left += 1
                 right -=1
             else:
+                #If both side does not match its not a palindrome
                 return False
+        #If there is no returns it's a polindrome
         return True
 
 
 def is_palindrome_recursive(text, left=None, right=None):
     # TODO: implement the is_palindrome function recursively here
-    pass
     # once implemented, change is_palindrome to call is_palindrome_recursive
     # to verify that your iterative implementation passes all tests
+    if left is None and right is None:
+        text = remove_puctuation(text)
+        print("This is the text: {}".format(text))
+    if len(text) < 1 or text == '':
+            return True
+
+    if left is None and right is None:
+        left = 0
+        right = len(text) -1
+    print("This is the indexes: {}, {}".format(left, right))
+
+    if left <= right:
+        if text[left] == text[right]:
+            is_palindrome_recursive(text, left=left + 1, right=right -1)
+        else:
+            print("Sorry this is not a palindrome...")
+            return False
+    print("This is a palindrome...")
+    return True
+
+def remove_puctuation(text):
+    return ''.join([i for i in text.upper() if i in string.ascii_letters])
 
 
 def main():
