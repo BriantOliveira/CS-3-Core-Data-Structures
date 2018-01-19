7ab727a67982e3c7bbe202f88bbe55a71dbe533f
Briant Oliveira <briantmoliveira@gmail.com>
2018-01-19 01:28:33 -0800

    Optional methods added

--- source/stack.py
+++ source/stack.py
@@ -22,20 +22,46 @@ class LinkedStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        return self.head is None
+
+    def size(self):
+        return self.size
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
 
+
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Push given item
+        new_node = self.list
+        #Give the data to the new node
+        new_node.data = data
+
+        # if self.head:
+        self.head = new_node
+        self.head.next = next_node
+        self.size +=1
+
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
+
+        # Check if the head is empty
+
+        # if  self.head:
+        #     return self.head.data
+        # else:
+        #     return None
+
+        if self.is_empty():
+            return None
+        else:
+            return self.head.data
 
     def pop(self):
         """Remove and return the item on the top of this stack,
