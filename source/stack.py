d63ff48f267b9df87b7b63d8d952bc324e6a848c
Briant Oliveira <briantmoliveira@gmail.com>
2018-01-19 02:27:15 -0800

    debuging the data

--- source/stack.py
+++ source/stack.py
@@ -15,13 +15,6 @@ class LinkedStack(object):
             for item in iterable:
                 self.push(item)
 
-    # def __str__(self):
-    #     return_str = ''
-    #     cur_node = self.head
-    #     while cur_node is not None:
-    #         return_str += str(cur_node.data)
-    #         current_none = cur_node.next
-    #     return return_str
 
     def __repr__(self):
         """Return a string representation of this stack."""
@@ -30,7 +23,7 @@ class LinkedStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
-        return self.head is None
+        return self.list.head is None
 
     def size(self):
         return self.size
@@ -38,31 +31,38 @@ class LinkedStack(object):
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
-        #Node count initialized to zero
-        node_count = 0
-        #Start at the head node
-        node = self.head
-        #Loop until the node is None, which is one node far past the tail
-        while node is not None:
-            #Count one for this node
-            node_count += 1
-            #Skip to the next node
-            node = node.next
-        #Now node count contain the amount of nodes
-        return node_count
+
+        return self.list.length()
+
+
+        # #Node count initialized to zero
+        # node_count = 0
+        # #Start at the head node
+        # node = self.list.head
+        # #Loop until the node is None, which is one node far past the tail
+        # while node is not None:
+        #     #Count one for this node
+        #     node_count += 1
+        #     #Skip to the next node
+        #     node = node.next
+        # #Now node count contain the amount of nodes
+        # return node_count
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Push given item
-        new_node = self.list
-        #Give the data to the new node
-        new_node.data = data
+        # new_node = self.list
+        # #Give the data to the new node
+        # new_node.data = data
+        #
+        # # if self.head:
+        # self.head = new_node
+        # self.head.next = next_node
+        # self.size +=1
 
-        # if self.head:
-        self.head = new_node
-        self.head.next = next_node
-        self.size +=1
+        #prepend to the head
+        self.list.prepend(item)
 
 
 
@@ -81,8 +81,9 @@ class LinkedStack(object):
 
         if self.is_empty():
             return None
-        else:
-            return self.head.data
+
+        return self.list.head.data
+
 
     def pop(self):
         """Remove and return the item on the top of this stack,
@@ -94,14 +95,22 @@ class LinkedStack(object):
         if self.is_empty():
             return None
         #remove and return object to the top
-        else:
-            first_element = self.head
-            self.head = first_element.next
-            #Decrement the value of the size
-            self.size -= 1
-            #return data
-            return first_element.data
+        # else:
+        #     first_element = self.head
+        #     self.head = first_element.next
+        #     #Decrement the value of the size
+        #     self.size -= 1
+        #     #return data
+        #     return first_element.data
 
+        data = self.list.head.data
+
+        if self.list.tail == self.list.head:
+            self.list.head = None
+            self.list.tail = None
+        else:
+            self.list.head = self.list.head.next
+        return data
 
 # Implement ArrayStack below, then change the assignment at the bottom
 # to use this Stack implementation to verify it passes all tests
