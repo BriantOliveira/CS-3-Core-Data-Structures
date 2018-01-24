f73bdeda523b5b812eccb795bac9fc5e74ce48cd
Briant Oliveira <briantmoliveira@gmail.com>
2018-01-24 15:09:44 -0800

    intersection and difference

--- source/set.py
+++ source/set.py
@@ -10,20 +10,23 @@ class Set(object):
 
     def size(self):
         """Returns if its True, else is False"""
+        #Return the size of the set
         return self.size
 
     def contains(self, element):
         """
         return a boolean indicating whether element is in this set
         """
+        #Checking if the element is in the set
         contain = self.data.contains(element)
         return contain
 
     def add(self, element):
         """ Add element to this set, if not present already """
-
+        #Check is there is a element in the set
         if element is not in self.data.keys():
              self.data.set(element, None)
+             # If the set is none add 1
              self.size += 1
         else:
             return ValueError("The element in set exit already")
@@ -31,22 +34,26 @@ class Set(object):
     def remove(element):
         """Remove element from this set, if present, or else raise KeyError"""
 
+        #If the element is in the set
         if element is in self.data.keys():
+            # Delete the element
             self.data.delete(element)
+            #Resize the set
             self.size -= 1
         else:
             return ValueError('The element does not exist in set')
 
     def union(self, other_set):
         """return a new set that is the union of this set and other_set"""
-
+        #Create a new variable for the new set
         new_set = Set(self.data.keys())
-
+        #Check is the other_set contain the element
         for element in other_set.data.keys():
             if self.data.contains(element):
                 continue
 
         else:
+            #Add the new element to the new_set
             new_set.data.append(element)
 
         return new_set
@@ -55,11 +62,14 @@ class Set(object):
         """
         return a new set that is the intersection of this set and other_set
         """
+        #Check if the set is empty
         if other_set.data and self.data is None:
             new_set = Set()
 
+            #Check if the set contain the element that we are looking for
             for element in self.data.keys():
                 if self.data.contains(element):
+                    #Add the element to the new set
                     new_set.data.append(element)
                 return new_set
             else:
@@ -67,8 +77,13 @@ class Set(object):
 
     def difference(other_set):
         """ return a new set that is the difference of this set and other_set """
-
+        #Check if the set is empty
         if other_set.data and self.data is None:
             new_set = Set()
+            #Check if the set contains the element, if so remove 
+            for element in self.data.keys():
+                if self.data.contains(element):
+                    new_set.data.remove(element)
 
-            for element in self
+            new_set = self.data.keys()
+            return new_set
