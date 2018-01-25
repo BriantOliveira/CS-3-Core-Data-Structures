9429f4aa12056d720cb8b75fcddad081fad21619
Briant Oliveira <briantmoliveira@gmail.com>
2018-01-25 00:40:40 -0800

    added more

--- source/circularbuffer.py
+++ source/circularbuffer.py
@@ -20,3 +20,26 @@ class CircularBuffer(object):
     if self.data not is_empty():
         if len(self.data) == self.max:
         return ValueError('Buffer is full...')
+
+    def enqueue(self, item):
+        #insert item at the back of the buffer
+        self.max.append(item)
+
+    def front(self):
+        #return the item at the front of the buffer
+        if self.is_empty():
+            return None
+        return self.max.head.data
+
+    def dequeue(self):
+        #remove and return the item at the front of the buffe
+        if self.is_empty():
+            raise ValueError
+        data = self.max.head.data
+
+        if self.max.tail == self.max.head:
+            self.max.head = None
+            self.max.tail = None
+        else:
+            self.max.head = self.max.head.next
+        return data
