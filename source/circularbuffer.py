92eb7f831e87db2bbc27b85a09ef9d19da3c55a0
Briant Oliveira <briantmoliveira@gmail.com>
2018-01-24 23:39:56 -0800

    some done

--- source/circularbuffer.py
+++ source/circularbuffer.py
@@ -2,6 +2,21 @@ from hashtable  import HashTable
 
 
 class CircularBuffer(object):
-
+    #initialize a new circular buffer that can store at most max_size items
     def __init__(self, max_size):
-        self.max_size = 7  
+        self.max = max_size
+        self.data = []
+
+    #property that tracks the number of items in the buffer
+    def size(self):
+        return self.size
+
+    #check if the buffer is empty
+    def is_empty(self):
+        return self.head is None
+
+    def is_full(self):
+    #check if the buffer is full
+    if self.data not is_empty():
+        if len(self.data) == self.max:
+        return ValueError('Buffer is full...')
